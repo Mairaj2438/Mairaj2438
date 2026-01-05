7287adf3609ea3e4d94c8583f5bc3ac5daf62c29
Mairaj2438 <152067045+Mairaj2438@users.noreply.github.com>
2026-01-05 10:56:22 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+// appproximately all math functions in the javascript:
+console.log(Math.abs(-4))
+
+console.log(Math.ceil(0.5489,9))
+console.log(Math.floor(34,34,22,11,89))
+console.log(Math.round(0.4591))
+console.log(Math.min(23,45,65))
+console.log(Math.max(567,23,11,2,90))
+
+
+
+console.log(Math.random());
+console.log((Math.random()*7)+23)
+
+
+const min=10
+const max =20
+
+console.log(Math.floor(Math.random( )*(max-min+1))+min)
+
+
+
